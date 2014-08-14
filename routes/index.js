fa7432bf1349d7b65524eb03850e5fc9bfefc78f
hamidgolive <hamid_hoseini@aol.com>
2014-08-13 17:42:06 -0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+
+var Candidate = mongoose.model('Candidates', {
+    name: 'string',
+    email: 'string'
+});
+
+router.get('/candidates', function(req, res){
+    Candidate.find({}, function(err, data){
+        //console.log(req.params.id);
+        res.json(data);
+    });
+});
+router.get('/candidates/:id', function(req, res){
+    Candidate.find({'_id':req.params.id}, function(err, data){
+        console.log(req.params.id);
+        res.json(data);
+    });
+});
+router.post('/candidates', function (req, res) {
+    console.log(req.body);
+
+    (new Candidate({
+        name: req.body.name,
+        email: req.body.email
+    })).save(function (err,data) {
+            console.log(err);
+            console.log(data);
+            if (err) {
+                res.json(500, { message: 'Could not connect to the database.'});
+            } else {
+                res.json(200, { message: 'Succesfully updated data ... ' });
+            }
+        });
+});
+router.put('/candidates/:id', function (req, res) {
+    console.log(req.body);
+
+    Candidate.findById(req.params.id, function (err, Candidate) {
+
+        if (err)
+            res.send(err);
+
+        Candidate.name = req.body.name; 	// update the bears info
+        Candidate.email = req.body.email;
+
+        Candidate.save(function (err) {
+            if (err) {
+                res.json(500, { message: 'Could not connect to the database.'});
+            } else {
+                res.json(200, { message: 'Succesfully updated data ... ' });
+            }
+        });
+
+    });
+});
+router.delete('/candidates/:id', function (req, res) {
+    console.log(req.body);
+
+    Candidate.remove({
+        _id: req.params.id
+    }, function(err, data) {
+        if (err) {
+            res.json(500, { message: 'Could not connect to the database.'});
+        } else {
+            res.json(200, { message: 'Succesfully deleted data ... ' });
+        }
+    });
+
+});
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
